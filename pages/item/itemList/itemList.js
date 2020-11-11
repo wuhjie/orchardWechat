418ad9a6642bcb18a70b404f90cf923b9c9c8a28
wuhjie <whjie001110@outlook.com>
2020-11-11 17:25:07 +0800

    1111/3rd

---
+++ pages/item/itemList/itemList.js
@@ -0,0 +1,45 @@
+// pages/item/itemList/itemList.js
+const util = require('../../utils/utils.js')
+var page = 0
+var totalPages = 3
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+
+  data: {
+    logs: [],
+    spinShow: true,
+    loadMore: true,
+    loadMoreTips: 'loading',
+    allLoaded: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.clearCache();
+    this.getArticles(0);
+  },
+
+  getArticles: function(pageno) {
+    let vm = this
+    setTimeout(function() {
+      vm.setData({
+        spinShow: false
+      })
+      var res = (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    }) 
+    // this.setData({
+    //   logs: (wx.getStorageSync('logs') || []).map(log => {
+    //     return Uint8ClampedArray.formatTime(new Date(log))
+    //   })
+    // })
+  },
+})
