3b6b0333d531943077f1e2773c5c4e9681f9081c
wuhjie <whjie001110@outlook.com>
2020-11-12 15:18:14 +0800

    11/12

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,13 +7,21 @@ Page ({
     imgUrls: [
       '../../images/index/index_1.JPG',
       '../../images/index/index_2.JPG',
-      '../../images/index/index_3.JPG'
+      '../../images/index/index_3.JPG',
     ],
+    cardList:[],
 
     indicatorDots: true,
     autoplay: true,
     interval: 2000,
-    duration: 1000,
+    duration: 10000,
+    
+    isAdd: false,
+    handleAdd() {
+      this.setData({
+        isAdd: !this.data.isAdd
+      })
+    }
   },
   
   customerLogin:function() {
@@ -47,31 +55,14 @@ Page ({
 
   itemInfo:function() {
     var that = this;
-    wx.request({
-      url: 'http://localhost:8080/company',
-      method: 'Get',
-      header :{
-        'content-type': 'application/json'
-      },
-      success: function (req) {
-        var list = req.data;
-        if (list == null) {
-          wx.showToast({
-            title: 'ErrorMsg',
-            icon: 'false',
-            duration: 1500
-          })
-        } else {
-          that.setData({
-            list: list
-          })
-        }
-      }
+    wx.navigateTo({
+      // event when clicking item info
+      url: '../item/itemList/itemList',
     })
   },
 
   onLoad: function() {
-
+    this.
   },
 
 })
--- pages/item/itemList/itemList.js
+++ pages/item/itemList/itemList.js
@@ -1,5 +1,4 @@
 // pages/item/itemList/itemList.js
-const util = require('../../utils/utils.js')
 var page = 0
 var totalPages = 3
 
@@ -22,24 +21,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.clearCache();
-    this.getArticles(0);
+    // this.clearCache();
+    // this.getArticles(0);
   },
 
-  getArticles: function(pageno) {
-    let vm = this
-    setTimeout(function() {
-      vm.setData({
-        spinShow: false
-      })
-      var res = (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    }) 
-    // this.setData({
-    //   logs: (wx.getStorageSync('logs') || []).map(log => {
-    //     return Uint8ClampedArray.formatTime(new Date(log))
-    //   })
-    // })
-  },
+//   getArticles: function(pageno) {
+//     let vm = this
+//     setTimeout(function() {
+//       vm.setData({
+//         spinShow: false
+//       })
+//       var res = (wx.getStorageSync('logs') || []).map(log => {
+//         return util.formatTime(new Date(log))
+//       })
+//     }) 
+//     // this.setData({
+//     //   logs: (wx.getStorageSync('logs') || []).map(log => {
+//     //     return Uint8ClampedArray.formatTime(new Date(log))
+//     //   })
+//     // })
+//   },
 })
